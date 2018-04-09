a3d5985a68caeb3119d8ae93a1a1d1fafe03103d
Victor Ordu <victorordu@outlook.com>
2018-04-09 17:28:08 +0100

    Updated tests

--- tests/testthat/test-sensitisation.R
+++ tests/testthat/test-sensitisation.R
@@ -1,6 +1,6 @@
 # test-sensitisation.R
 
-context("Check interactive display")
+context("Interactive display")
 
 test_that("Improper data input is detected", {
   expect_error(display_data(42))
@@ -15,4 +15,12 @@ test_that("Improper data input is detected", {
 test_that('imported data has the correct structure', {
   expect_true(file.exists('../test-dat.csv'), info = "Test file is missing")
   expect_error(chartApp('../test-dat.csv'))
+})
+
+
+context("Bar Chart Plotting")
+
+test_that("open ended questions are filtered out", {
+  expect_error(discard_comments(matrix(1:12)))
+  expect_error(discard_comments(list(a = 1:10, b = LETTERS[1:10])))
 })
