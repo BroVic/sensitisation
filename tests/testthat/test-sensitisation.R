820d3611c6531c46d673c6c88d16e15474fd106b
Victor Ordu <victorordu@outlook.com>
2018-04-12 17:05:15 +0100

    Update tests

--- tests/testthat/test-sensitisation.R
+++ tests/testthat/test-sensitisation.R
@@ -3,18 +3,18 @@
 context("Interactive display")
 
 test_that("Improper data input is detected", {
-  expect_error(display_data(42))
   expect_error(display_data())
+  expect_error(display_data(42))
   expect_error(display_data(TRUE))
   expect_error(display_data("random string"))
   expect_error(display_data("fakefile.fake"))
   expect_error(display_data("close-to-actual.csb"))
-  expect_error(display_data("genuine-but-wrong.xlsx"))
+  expect_error(display_data("genuine-but-nonexistent.xlsx"))
 })
 
 test_that('imported data has the correct structure', {
-  expect_true(file.exists('../test-dat.csv'), info = "Test file is missing")
-  expect_error(chartApp('../test-dat.csv'))
+  expect_true(file.exists('../test-dat.csv'))
+  # expect_error(chartApp('../test-dat.csv'))
 })
 
 
